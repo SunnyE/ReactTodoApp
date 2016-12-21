ab813654d5b88919568518d43fa3d9bbfc3592b2
Ethan Favia <efavia215@live.com>
2016-12-21 13:10:26 -0500

    passing the dummy data down the tree

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var Todo = require('Todo');
 
 var TodoList = React.createClass({
     render: function () {
+        var {todos} = this.props;
+        var renderTodos = () => {
+            return todos.map((todo) => {
+                return (
+                    <Todo key={todo.id}/>
+                )
+            });
+        }
         return (
             <div>
-                test
+                {renderTodos()}
             </div>
         )
     }
