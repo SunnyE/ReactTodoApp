0336f3f1029c16288cb89d0eb82e746eb45575af
Ethan Favia <efavia215@live.com>
2016-12-23 19:03:26 -0500

    some fixes to the package.json and some new tests

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,25 @@ var expect = require('expect');
 var $ = require('jquery');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo'); 
 
-describe('Todo', () => {
+describe('TodoList', () => {
     it('should exist', () => {
         expect(TodoList).toExist(); 
-    })
+    });
+    it('should render a todo component for every todo item', () => {
+        var todos = [
+            {
+                id:1,
+                text: 'Read stock book'
+            },
+            {
+                id:2,
+                text: 'get those things done'
+            }];
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+        var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+        expect(todosComponents.length).toBe(todos.length)
+    });
 });
