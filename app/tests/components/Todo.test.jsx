6c3d6d59bb4e3aa75f60461372268e07fb4d3639
Ethan Favia <efavia215@live.com>
2016-12-30 10:47:13 -0500

    tests added in todoApp and todo

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -9,5 +9,24 @@ var Todo = require('Todo');
 describe('Todo', () => {
     it('should exist', () => {
         expect(Todo).toExist(); 
+    });
+
+    it('should call onToggle prop with id on click', () => {
+        var todoDate = {
+            id: 201,
+            text: 'this is a test for test sale',
+            completed: true
+        };
+
+        var spy = expect.createSpy();
+        var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+
+        var $el = $(ReactDOM.findDOMNode(todo));
+
+        TestUtils.Simulate.click($el[0]);
+
+        expect(spy).toHaveBeenCalledWith(201);
+
+        
     })
 });
