62e2d8a894bd998610abcc4d970c6b5db04353aa
Ethan Favia <efavia215@live.com>
2017-01-04 15:02:51 -0500

    tests for the completed todo filter

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -22,6 +22,17 @@ module.exports = {
     filterTodos: function(todos, showCompleted, searchText) {
         var filteredTodos = todos;
 
+        //filter by showCompleted
+        filteredTodos = filteredTodos.filter(() => {
+            return !todo.completed || showCompleted;
+        })
+
+        // filter by searchText
+
+
+        // sort todos with non-completed first
+
+
         return filteredTodos;
     } 
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -55,4 +55,33 @@ describe('TodoAPI', () => {
         expect(actualTodos).toEqual(todos); 
         });
     });
+
+    describe('filterTodos', () => {
+        var todos = [{
+            id: 1,
+            text:"sample text",
+            completed: true
+        },
+        {
+            id: 2,
+            text:"sample text 45235232",
+            completed: false
+        },
+        {
+            id: 3,
+            text:"sample text",
+            completed: true
+        }];
+        it('should return all if show completed is true', ()=> {
+        var filteredTodos = TodoAPI.fitler(todos, true, '');
+
+        expect(filteredTodos.length).toBe(3);
+        });
+
+        it('should return 1 if show completed is false', ()=> {
+        var filteredTodos = TodoAPI.fitler(todos, false, '');
+
+        expect(filteredTodos.length).toBe(1);
+        });
+    });
 });
