bbba558842cd71fbb3b19af88a1f4f8a7c3ab329
Ethan Favia <efavia215@live.com>
2017-01-09 11:32:23 -0500

    updated package.json and a mess around with moment

---
+++ playground/moment-examples.js
@@ -0,0 +1,18 @@
+var moment = require('moment');
+
+console.log(moment().format());
+
+
+
+var now = moment();
+
+console.log('current timestamp', now.unix());
+
+var timestamp = 1459111648;
+
+var currentMoment = moment.unix(timestamp);
+console.log('current moment', currentMoment.format('MMM D, YY @ H:mm A'));
+
+console.log('current moment', currentMoment.format('MMMM Do, YYYYY @ H:mm a'));
+
+
