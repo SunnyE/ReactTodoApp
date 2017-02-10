bae2c12427a643d425173ca66d5da72c0ca3bb4b
Ethan Favia <efavia215@live.com>
2017-02-10 15:41:14 -0500

    toggle todos and tests done

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -12,12 +12,33 @@ describe('Actions', ()=> {
         expect(res).toEqual(action); 
     });
 
+    it('should generate toggle show completed action', () => {
+        var action = {
+            type: 'TOGGLE_SHOW_COMPLETED'
+        }
+
+        var res = actions.toggleShowCompleted();
+
+        expect(res).toEqual(action); 
+    })
+
+
     it('should generate add todo action', () => {
         var action = {
             type: 'ADD_TODO',
             text: 'Things to do later'
         };
         var res = actions.addTodo(action.text);
+
+        expect(res).toEqual(action);
+    });
+
+    it('should generate toggle todo action', () => { 
+        var action = {
+            type: 'TOGGLE_TODO',
+            id: '123'
+        };
+        var res = actions.toggleTodo(action.id);
 
         expect(res).toEqual(action);
     });
