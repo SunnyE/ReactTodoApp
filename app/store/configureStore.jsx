0f297819bfd62691c6f3b6acb26c9a16fa180b95
Ethan Favia <efavia215@live.com>
2017-02-22 15:48:53 -0500

    small fix

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -10,5 +10,7 @@ export var configure = () => {
 
     var store = redux.createStore(reducer, redux.compose(
         window.devToolsExtension ? window.devToolsExtension(): f => f
-    ))
+    ));
+
+    return store;
 }
